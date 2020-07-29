d48a47c4c186feab1f0f5c5a16f912ecf2b7d8d6
Karol Etrych <karol.etrych@gmail.com>
2020-07-29 20:01:34 +0200

    Fix Core compilation

--- src/Core/Parsing/CsvTransactionParser.cs
+++ src/Core/Parsing/CsvTransactionParser.cs
@@ -1,7 +1,12 @@
+using System.IO;
+
 namespace ExpenseAnalyzer.Core
 {
     class CsvTransactionParser : ITransactionParser
     {
-        
+        public ImportedTransactions Parse(Stream inputFileStream)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- src/Core/Parsing/ITransactionParser.cs
+++ src/Core/Parsing/ITransactionParser.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace ExpenseAnalyzer.Core
 {
     public interface ITransactionParser
