eafe9c542aa4147120fad06ad7c4b1d4ce7403ec
Karol Etrych <karol.etrych@gmail.com>
2020-07-29 20:00:32 +0200

    move gitignore

--- src/Core/Transaction.cs
+++ src/Core/Transaction.cs
@@ -5,6 +5,6 @@ namespace ExpenseAnalyzer.Core
     public class Transaction
     {
         public decimal Amount { get; set; }
-        public Date Date { get; set; }
+        public DateTime Date { get; set; }
     }
 }
