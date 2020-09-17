cf84158b5d18474460f4a0a6a77b681191778b58
Karol Etrych <karol.etrych@gmail.com>
2020-09-17 15:03:31 +0200

    start ImportFile controller action implementation

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,24 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Dropzone from 'react-dropzone'
+
+
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+        {({getRootProps, getInputProps}) => (
+            <section>
+            <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+            </section>
+        )}
+        </Dropzone>
       </header>
-    </div>
+      </div>
+      
   );
 }
 
